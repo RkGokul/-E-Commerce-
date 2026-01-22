74977e379dffaca69669f2ca2f6c6ee60ccecb95
Gokul <your-email@example.com>
2026-01-22 18:38:24 +0530

    config changes 2

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 
 export default defineConfig({
   server: { host: true },
   base: '/',
+  plugins: [react()],
 })
+
+// export default defineConfig({
+//   plugins: [react()],
+// });
